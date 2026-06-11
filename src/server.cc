726dd3c22126f1ca0bf5f6c2f4d91f64964ab177
shane <shaneherreraso@gmail.com>
2026-06-11 15:24:45 -0700

    build: include netinet posix package

--- src/server.cc
+++ src/server.cc
@@ -1,18 +1,23 @@
 #include <arpa/inet.h>
-#include <iostream>
+#include <cerrno>
 #include <cstring>
 #include <cstdint>
-#include <cerrno>
+#include <iostream>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <stdexcept>
 
 int main() {
+
+  // 
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   int opt_value = 1;
 
   int error_number = 0;
+
+  // Set socket options
 
   try {
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt_value, sizeof(opt_value)) == -1) {
@@ -24,6 +29,8 @@ int main() {
     std::cerr << "Error: " << e.what() << std::endl;
     std::cerr << "Log: " << std::strerror(error_number) << std::endl;
   }
+
+  // Bind selected IP address and port to socket
 
   return 0;
 
