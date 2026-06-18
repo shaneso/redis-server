6326336d2ace492d0256e4ec65de5ca7558f673f
shane <shaneherreraso@gmail.com>
2026-06-17 22:45:52 -0700

    feat: modify project struct and add inline log

--- src/util/logging.h
+++ src/util/logging.h
@@ -1,9 +1,12 @@
+#pragma once
+
+#ifndef ERR_H
+#define ERR_H
+
 // stdlib
 #include <cerrno>
 #include <cstring>
 #include <iostream>
-// project
-#include "err.h"
 
 /**
  * @brief Formats and logs an error message corresponding to a failed process.
@@ -11,8 +14,10 @@
  * @param exit_code is the exit value
  * @param message is the error message log
  */
-void err(int exit_code, const char* message) {
+inline void err(int exit_code, const char* message) {
   std::cerr << "[" << message << "] " << std::strerror(errno) << std::endl;
   std::exit(exit_code);
 }
+
+#endif // ERR_H
 
