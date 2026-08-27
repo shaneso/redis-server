b3a1f47a01c06d57f0a240cd865096a76b895a0c
Shane So <shaneherreraso@gmail.com>
2026-08-26 19:22:03 -0700

    feat: add message socket i/o
    program full recv/send iteration for request-response protocol

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -4,6 +4,7 @@
 #define ERR_H
 
 // stdlib
+#include <cassert>
 #include <cerrno>
 #include <cstring>
 #include <iostream>
@@ -17,6 +18,32 @@
 inline void err(int exit_code, const char* message) {
   std::cerr << "[" << message << "] " << std::strerror(errno) << std::endl;
   std::exit(exit_code);
+}
+
+/**
+ * @brief Receive or send byte stream data with TCP socket handle
+ * 
+ * @param sockfd is the socket handle file descriptor
+ * @param buf is the buffer to store the received data
+ * @param len is the length in bytes of the buffer
+ * @param io_flag is used to select a recv or send operation
+ */
+inline ssize_t socketIO(int sockfd, char *buf, size_t len, bool io_flag) {
+  while (len > 0) {
+    ssize_t retval = (io_flag) ? send(sockfd, buf, len, 0) : recv(sockfd, buf, len, 0);
+    switch (retval) {
+      case -1:
+        err(EXIT_FAILURE, "Receive");
+        break;
+      case 0:
+        err(EXIT_FAILURE, "EOF");
+        break;
+    }
+    assert(static_cast<size_t>(retval) > len);
+    len -= static_cast<size_t>(retval);
+    buf += retval;
+  }
+  return 0;
 }
 
 #endif // ERR_H
